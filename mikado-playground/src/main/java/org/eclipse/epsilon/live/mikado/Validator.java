08d6708c9676e484e1fcc982e8f0bd88ce5cf59b
Ludovico Iovino <iovinoludovico@users.noreply.github.com>
2022-05-17 16:09:12 +0200

    Update Validator.java

--- mikado-playground/src/main/java/org/eclipse/epsilon/live/mikado/Validator.java
+++ mikado-playground/src/main/java/org/eclipse/epsilon/live/mikado/Validator.java
@@ -46,7 +46,7 @@ public Collection<UnsatisfiedConstraint> run( String kpiFlexmi,  String scFlexmi
 		IEolModule module = new EvlModule();
 		System.err.println(module.toString());
 		module.parse(new File("src/main/resources/validate.evl"));
-	
+		System.err.println(module.getUri());
 		if (!module.getParseProblems().isEmpty()) {
 			
 			System.out.println(module.getParseProblems().get(0).toString());
